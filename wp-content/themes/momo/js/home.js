68b4aec464b73b809ed18490b2271af381e8f587
ron williams <nameofemail@gmail.com>
2012-03-01 01:52:17 -0500

    made some simple UI changes - sliding comment forms on the home pag

--- wp-content/themes/momo/js/home.js
+++ wp-content/themes/momo/js/home.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+    // For truncated content on the home page - When the user clicks the 
+    // expand button, expand to the height of the content, 
+    // Toggles back to the original height. 
     $('.entry-content').each(function(){
         var h = $(this).find('p').height(); 
         var h1 = $(this).height(); 
@@ -20,4 +23,18 @@ $(document).ready(function(){
             $(elem).removeClass('collapse').addClass('expand'); 
         }
     }
+    // Pop down the hidden comments form on the home page. 
+    $('.comments_home_show_btn').each(function(key, val){
+        $(this).attr('data-toggle', 0); 
+    });
+    $('.comments_home_show_btn').on('click', function(){
+        if (parseInt($(this).attr('data-toggle')) % 2 == 0) {
+            $(this).next('.comments_home_hide').find('.respond').slideDown(300); 
+        } else {
+            $(this).next('.comments_home_hide').find('.respond').slideUp(300); 
+        }
+        var num = $(this).attr('data-toggle'); 
+        num ++; 
+        $(this).attr('data-toggle', num); 
+    });
 });
